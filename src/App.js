31c8cdb3b349a84e0bb24f42ffcb2c1615616f8d
Hitesh Khandelwal <hiteshkhandelwal2002@gmail.com>
2024-08-10 21:09:59 +0530

    .

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
-  pageSize = 5;
+  pageSize = 6;
 
   render() {
     return (
@@ -20,7 +20,7 @@ export default class App extends Component {
             <Route exact path="/" element={<News key="general" pageSize={this.pageSize} country="in" category="general" />} />
             <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="business" />} />
             <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" />} />
-            <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="general" />} />
+            {/* <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="general" />} /> */}
             <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="health" />} />
             <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="science" />} />
             <Route exact path="/sports" element={<News key="sports" pageSize={this.pageSize} country="in" category="sports" />} />
