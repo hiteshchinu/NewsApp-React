5475855aecca0530bea0df4dea18d30f27b3da0a
Hitesh Khandelwal <hiteshkhandelwal2002@gmail.com>
2024-08-10 20:10:08 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
-import NavBar from './components/NavBar';
+import NavBar from './Components/NavBar';
 import News from './Components/News';
 import {
   BrowserRouter as Router,
