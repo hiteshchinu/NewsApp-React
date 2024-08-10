5da4c076273523e8e7f27746674725b5b6f4aae5
Hitesh Khandelwal <hiteshkhandelwal2002@gmail.com>
2024-08-11 00:48:23 +0530

    .

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,9 +6,9 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card">
-                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}> {source}
-                        </span>
-                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                {/* <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}> {source}
+                        </span> */}
+                    <img src={!imageUrl ? "https://media.istockphoto.com/id/1387606902/vector/latest-news-label-with-megaphone-breaking-news-announce-loudspeaker-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=3u2wix939RRD52zitMt5WcWQJvL_HRyDrR4Pyryz0h4=" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}  </h5>
                         <p className="card-text">{description}</p>
